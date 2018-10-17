ae35accee65412c77d9a1ea3d8dc6b7a6959a274
Chris Barrick <cbarrick1@gmail.com>
2018-10-17 05:07:33 -0400

    Improve sync_problems.sh when working directory is dirty

--- sync_problems.sh
+++ sync_problems.sh
@@ -3,8 +3,8 @@ function sync_problems {
 	local STASH=`git stash create`
 	git reset --hard
 	git submodule update --remote _problems
-	git commit -am 'Sync with problem archive'
-	[ -z $STASH ] && git stash apply $STASH
+	git commit -am 'Sync with problem archive' || true
+	[ -z $STASH ] || git stash apply $STASH
 }
 
 sync_problems
