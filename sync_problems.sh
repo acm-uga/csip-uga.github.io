29a6f565ce3e94f2293fee8b83d24795ac1ff349
Chris Barrick <cbarrick1@gmail.com>
2018-10-17 04:53:11 -0400

    Improve sync_problems.sh when working directory is clean

--- sync_problems.sh
+++ sync_problems.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 function sync_problems {
 	local STASH=`git stash create`
-	echo $STASH
 	git reset --hard
 	git submodule update --remote _problems
 	git commit -am 'Sync with problem archive'
-	git stash apply $STASH
+	[ -z $STASH ] && git stash apply $STASH
 }
 
 sync_problems
